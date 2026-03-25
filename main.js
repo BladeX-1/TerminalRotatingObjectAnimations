34eb09a4cd26636bdc19a23472c5eeca3c69ce53
Abhishek <abhi.abhishek2311@gmail.com>
2026-03-25 09:04:52 +0000

    handle case when object crosses over the board boundary

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ let pointsXYZ, normalXYZ, xmin, xmax, ymin, ymax;
 let boardHeight = 100;
 let boardWidth = 100;
 
-while(true){
+while (true) {
 
     for (let theta = 0; theta < Math.PI * 2; theta += 0.02) {
         let [board, zbuffer] = getEmptyBoard(boardHeight, boardWidth);
@@ -29,26 +29,23 @@ while(true){
             let [x, y, z] = pointsXYZ[i];
             [x, y, z] = xrot(x, y, z, theta);
             [x, y, z] = yrot(x, y, z, 2 * theta);
-            
+
             let [nx, ny, nz] = normalXYZ[i];
             [nx, ny, nz] = xrot(nx, ny, nz, theta);
             [nx, ny, nz] = yrot(nx, ny, nz, 2 * theta);
-            
+
             let xi = linearMapping(xmin - 0.1, 0, xmax + 0.1, boardWidth - 1, x);
             let yi = linearMapping(ymin - 0.1, 0, ymax + 0.1, boardHeight - 1, y);
-            try {
+            if (0 <= yi && yi < board.length && 0 <= xi && xi < board[0].length) {
                 if (zbuffer[yi][xi] == null || zbuffer[yi][xi] < z) {
-                    board[yi][xi] = getLuminosityChar(nx, ny, nz);
+                    board[yi][xi] = getLuminosity(nx, ny, nz);
                     zbuffer[yi][xi] = z;
                 }
-            } catch (TypeError) {
-                console.log(zbuffer, yi, xi, x, y);
-                throw TypeError;
             }
         }
         clearScreen();
         printBoard(board);
         await delay(10);
     }
-    
+
 }
